b935be2d01299699e825b9c64e2d72ce64883584
Matt Raffel <mattr@kiva.org>
2021-08-23 09:38:53 -0600

    corrects one field
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- endpoints/fsp/Models/GetReportResult.cs
+++ endpoints/fsp/Models/GetReportResult.cs
@@ -5,7 +5,7 @@ namespace fsp.Models
     public class GetReportResult
     {
         public int id { get; set; }
-        public string fspId { get; set; }
+        public string tdcFspId { get; set; }
         public string reportId { get; set; }
         public DateTime requestDate { get; set; }
         public string content { get; set; }
